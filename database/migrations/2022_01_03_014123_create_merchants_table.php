27bd505bfb7197d5c12155136e77c069f029cde6
richguo <richguo@jetfuel.com.tw>
2022-01-03 09:51:32 +0800

    migrate: merchants

---
+++ database/migrations/2022_01_03_014123_create_merchants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->uuid('uid');
+            $table->string('name', 100);
+            $table->integer('user_id');
+            $table->string('code', 50);
+            $table->dateTime('expired_at');
+            $table->tinyInteger('status');
+            $table->timestamps();
+
+            $table->primary('uid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+}
