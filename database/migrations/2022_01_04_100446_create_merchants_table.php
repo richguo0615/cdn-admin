e4f17dead4967299715000baa103ec9f3f36d1cd
richguo <richguo@jetfuel.com.tw>
2022-01-04 18:05:46 +0800

    update: migrations

---
+++ database/migrations/2022_01_04_100446_create_merchants_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchantsTable extends Migration
+{
+    private $tableName = 'merchants';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchants', function (Blueprint $table) {
+            $table->uuid('uid')->comment('商戶uid');
+            $table->string('name', 100)->comment('商戶名稱');
+            $table->unsignedBigInteger('user_id')->comment('mapping users.id');
+            $table->unsignedBigInteger('cdn_plan_id')->comment('mapping cnd_plans.id');
+            $table->unsignedBigInteger('default_line_id')->comment('mapping lines.id');
+            $table->unsignedBigInteger('deliver_domain_id')->comment('mapping domains.id');
+            $table->string('code', 50)->comment('商戶代碼');
+            $table->dateTime('expired_at')->comment('到期日');
+            $table->tinyInteger('status')->comment('狀態');
+            $table->timestamps();
+
+            $table->primary('uid');
+            $table->foreign('user_id', "fk_{$this->tableName}_user_id")->references('id')->on('users');
+            $table->foreign('cdn_plan_id', "fk_{$this->tableName}_cdn_plan_id")->references('id')->on('cdn_plans');
+            $table->foreign('default_line_id', "fk_{$this->tableName}_default_line_id")->references('id')->on('lines');
+            $table->foreign('deliver_domain_id', "fk_{$this->tableName}_deliver_domain_id")->references('id')->on('domains');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchants');
+    }
+}
